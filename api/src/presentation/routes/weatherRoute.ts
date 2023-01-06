63a2c6546e1dc442777fc3d6262690ee6a1ccd46
Gabriel Crevelario <terceiro-gcoliveira@icatuseguros.com.br>
2023-01-06 04:29:55 -0300

    fix: layout card

--- api/src/presentation/routes/weatherRoute.ts
+++ api/src/presentation/routes/weatherRoute.ts
@@ -4,13 +4,19 @@ import { container } from 'tsyringe';
 import { WeatherService } from '../../application/weatherService';
 import { WeatherRepository } from '../../infrastructure/repository/weatherRepository';
 import { WeatherController } from '../controller/weatherController';
+import cors from "cors";
 container.register('IWeatherRepository', { useClass: WeatherRepository }).resolve(WeatherRepository);
 container.register('IWeatherService', { useClass: WeatherService }).resolve(WeatherService);
 const weatherController = container.resolve(WeatherController);
-const routers = Router().get('/weathers', async (req: Request, res: Response) =>{ 
+const corsOptions = {
+    origin: [
+      'http://localhost:3000',
+    ],
+  };
+const routers = Router().get('/weathers', cors(corsOptions),async (req: Request, res: Response) =>{ 
     const weathers = await weatherController.findAll();
     res.send(weathers);
- }).get('/weathers/locale-name/:localeName', async (req: Request, res: Response) =>{ 
+ }).get('/weathers/locale-name/:localeName',cors(corsOptions), async (req: Request, res: Response) =>{ 
     const weathers = await weatherController.findByLocale(req);
     res.send(weathers);
  })
